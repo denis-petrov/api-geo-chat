6dea5ee6eb120370e6700e99523b5623c885a356
denis-petrov <den11032001@mail.ru>
2021-10-11 23:54:44 +0300

    Init commit

---
+++ src/main/java/com/app/apigeochat/ApiGeoChatApplication.java
@@ -0,0 +1,13 @@
+package com.app.apigeochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeoChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGeoChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/apigeochat/ApiGeoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.apigeochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
